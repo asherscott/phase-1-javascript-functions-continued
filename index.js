ff638886696e8a580312ff83427f37df9d9e36d1
asher scott <asher.scott6@gmail.com>
2022-02-23 09:46:28 -0600

    commit tree 126c2aac56675773ccefcc5aaf82420611a9c546

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function mondayWork(action) {
+    let monAction;
+    action === undefined
+    ? monAction = "This Monday, I will go to the office."
+    : monAction = `This Monday, I will ${action}.`;
+
+    return monAction;
+}
+
+function saturdayFun(action) {
+    let satAction;
+    action === undefined
+    ? satAction = "This Saturday, I want to roller-skate!"
+    : satAction = `This Saturday, I want to ${action}!`;
+
+    return satAction;
+}
+
+function wrapAdjective(flair = '*') {
+    return function(string) {
+            return `You are ${flair}${string}${flair}!`
+    }
+}
